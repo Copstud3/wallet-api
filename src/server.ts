9441ac8591435ba96ea5bd0d951a7af924b476ca
Chris <victorchris73@gmail.com>
2025-07-23 20:43:58 +0100

    Add cron job to send GET request every 14 minutes and update dependencies

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,18 @@ import express, { Application, Request, Response } from "express";
 import { initDB, PORT } from "./config/constants.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoutes from "./routes/transactions.route.js"
-
+import job from "../src/config/cron.js"
 
 const app: Application = express();
+
+if(process.env.NODE_ENV === "production") job.start();
 
 app.use(express.json());
 app.use(rateLimiter);
 
 
 app.get("/api/health", (req: Request, res: Response) => {
-    res.send("Activeeee like chivita!")
+    res.status(200).json({status: "ok"})
 })
 
 app.use("/api/transactions", transactionsRoutes)
